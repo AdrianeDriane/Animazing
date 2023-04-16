7d52eba4374ad04d29a03be6ce2611e11dfe2f08
Adriane Dilao <dilaoaparre@gmail.com>
2023-04-16 11:54:02 +0800

    fix: github link now opens new tab

--- src/components/Github.jsx
+++ src/components/Github.jsx
@@ -3,7 +3,11 @@ import { AiFillGithub } from 'react-icons/ai';
 
 export default function Github() {
   return (
-    <a href="https://github.com/AdrianeDriane/Animazing">
+    <a
+      href="https://github.com/AdrianeDriane/Animazing"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
       <AiFillGithub className="fixed top-10 right-16" size={50} color="white" />
     </a>
   );
