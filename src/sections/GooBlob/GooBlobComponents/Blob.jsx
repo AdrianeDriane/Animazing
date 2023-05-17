22e42fd37a8b16a2a33ad460f67e56a4ff61a4d0
AdrianeDriane <dilaoaparre@gmail.com>
2023-05-17 19:30:26 +0800

    feat: finished goo blob animation

--- src/sections/GooBlob/GooBlobComponents/Blob.jsx
+++ src/sections/GooBlob/GooBlobComponents/Blob.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { motion } from 'framer-motion';
 
-export default function Blob({ size }) {
+export default function Blob({ height, width, left, top, stiffness }) {
   return (
-    <div
-      className={`bg-lightCoral rounded-full inline-block h-28 w-28 shadow-blobBoxShadow opacity-60`}
-    ></div>
+    <motion.div
+      className={`bg-lightCoral rounded-full inline-block 
+      absolute ${height} ${width} shadow-blobBoxShadow opacity-60 z-10`}
+      layout
+      intial={{
+        left: 0,
+        top: 0,
+      }}
+      animate={{
+        left: left + 'px',
+        top: top + 'px',
+        transition: {
+          type: 'spring',
+          stiffness: stiffness,
+          damping: 8,
+        },
+      }}
+    ></motion.div>
   );
 }
 
 Blob.propTypes = {
-  size: PropTypes.string,
+  height: PropTypes.string.isRequired,
+  width: PropTypes.string.isRequired,
+  left: PropTypes.number,
+  top: PropTypes.number,
+  stiffness: PropTypes.number,
 };
--- src/sections/GooBlob/GooBlobSection.jsx
+++ src/sections/GooBlob/GooBlobSection.jsx
@@ -1,14 +1,57 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import Section from '../../templates/Section.jsx';
 import './GooBlob.css';
 import Blob from './GooBlobComponents/Blob.jsx';
 
 export default function GooBlobSection() {
+  const cursorRef = useRef(null);
+  const [blobLocationX, setBlobLocationX] = useState(0);
+  const [blobLocationY, setBlobLocationY] = useState(0);
+
+  const handleMouseMove = (event) => {
+    const cursor = cursorRef.current;
+    const rect = cursor.getBoundingClientRect();
+
+    cursor.style.left = event.clientX - rect.width / 2 + 'px';
+    cursor.style.top = event.clientY - rect.height / 2 + 'px';
+
+    setBlobLocationX(event.clientX);
+    setBlobLocationY(event.clientY);
+  };
+
+  useEffect(() => {
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
   return (
     <Section color="bg-white">
-      <div className="blobs text-center items-center justify-center flex relative h-full w-full top-0 left-0 right-0 bottom-0">
-        <Blob />
-        <Blob />
+      <div
+        className="blobs flex relative h-full w-full"
+        onMouseMove={handleMouseMove}
+      >
+        <div
+          ref={cursorRef}
+          className="bg-lightCoral rounded-full inline-block 
+      absolute h-10 w-10 shadow-blobBoxShadow opacity-60 z-10"
+        ></div>
+        <Blob
+          height="h-32"
+          width="w-32"
+          left={blobLocationX - 32 * 2}
+          top={blobLocationY - 32 * 2}
+          stiffness={60}
+        />
+        <Blob
+          height="h-20"
+          width="w-20"
+          left={blobLocationX - 20 * 2}
+          top={blobLocationY - 20 * 2}
+          stiffness={30}
+        />
       </div>
 
       <svg
