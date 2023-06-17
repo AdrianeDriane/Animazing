4523bb41b6d10be46ab650dbd418186a210596e7
Adriane Dilao <dilaoaparre@gmail.com>
2023-06-17 18:36:00 +0800

    feat: finished white cards animation

--- src/sections/WhiteCards/WhiteCardsComponents/WhiteCard.jsx
+++ src/sections/WhiteCards/WhiteCardsComponents/WhiteCard.jsx
@@ -2,15 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { motion } from 'framer-motion';
 
-export default function WhiteCard({ id }) {
+export default function WhiteCard({ value, isSpread }) {
   return (
     <motion.div
-      className="absolute bg-white h-96 w-72 shadow-whiteCardShadow 
-    shadow-slate-300 rounded-xl hover:cursor-pointer"
+      className={`absolute bg-white h-96 w-72 shadow-whiteCardShadow shadow-slate-300 rounded-xl hover:cursor-pointer`}
+      animate={{
+        translateX: isSpread ? `${value * 2}%` : `${value * 0.15}%`,
+        rotate: isSpread ? `${value}deg` : 0,
+        transition: {
+          type: 'spring',
+          damping: 8,
+        },
+      }}
     />
   );
 }
 
 WhiteCard.propTypes = {
-  id: PropTypes.number,
+  value: PropTypes.number.isRequired,
+  isSpread: PropTypes.bool.isRequired,
 };
